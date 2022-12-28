4ba6323bf07e3602aff42cf07bbd96b0ea4efaf3
vinaypatel1437 <66087753+vinaypatel1437@users.noreply.github.com>
2022-12-28 23:57:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+let inputData = document.getElementById("inputBox");
+let clearBtn = document.getElementById("clear");
+let button = document.querySelector("button");
+let toDoList = document.querySelector("ul");
+
+let arr = {};
+window.addEventListener('load', ()=>{
+    // arr = JSON.parse(localStorage.getItem("toDoData")) == null ? [] : JSON.parse(localStorage.getItem("toDoData"));
+    arr = JSON.parse(localStorage.getItem("toDoData")) || {};
+    // arr.forEach(element => {
+    //     toDoList.innerHTML += `<li>${element}</li>`;
+    // });
+    // for(let i=0;i<arr.length; i++) {
+    //     toDoList.innerHTML += `<li>${arr[i]} <button>delete</button></li>`;
+    // }
+    for(let i in arr) {
+        toDoList.innerHTML += `<li>${arr[i]}</li>`;
+    }
+    console.log(arr);
+})
+// let arrr = ["vinay", "patel", "abc"];
+
+button.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(inputData.value); 
+    let id = Math.random()*1000;      
+    // let child = document.createElement('li');
+    // console.log(child);
+    // child.innerText = inputData.value;
+    // toDoList.appendChild(child);
+    toDoList.innerHTML += `<li>${inputData.value}<button>delete</button></li>`;
+    // arr.push(inputData.value);
+    arr[id] = inputData.value;
+    localStorage.setItem("toDoData", JSON.stringify(arr));
+    inputData.value = "";
+})
+
+clearBtn.addEventListener("click", (e)=> {
+    e.preventDefault();
+    // localStorage.removeItem("toDoData");
+    // localStorage.clear();
+})
+// JSON.stringify converts any Object into String.
+// JSON.parse converts string into object.
